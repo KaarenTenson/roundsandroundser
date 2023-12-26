ec112cf00e5a8e21b8a65aad62fdf4f8cd3260e5
yeet nonono <kaurtenson123@gmail.com>
2023-12-26 02:25:35 +0200

    "

--- liikumine.gd
+++ liikumine.gd
@@ -11,7 +11,7 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta /2
+		velocity.y += gravity * delta
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
